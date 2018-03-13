85c4f28db7a26ae9c229609f2d82e0efdafb4a8b
Daniel Strøm <danielss89@gmail.com>
2018-03-13 19:00:13 +0100

    Fix user controller factory

--- src/ZfcUserImpersonate/Factory/Controller/UserControllerFactory.php
+++ src/ZfcUserImpersonate/Factory/Controller/UserControllerFactory.php
@@ -10,6 +10,15 @@ class UserControllerFactory
     {
         $redirectCallback = $container->get('zfcuser_redirect_callback');
         $controller = new \ZfcUserImpersonate\Controller\User($redirectCallback);
+        $controller->setServiceLocator($container);
+
+        $controller->setChangeEmailForm($container->get('zfcuser_change_email_form'));
+        $controller->setOptions($container->get('zfcuser_module_options'));
+        $controller->setChangePasswordForm($container->get('zfcuser_change_password_form'));
+        $controller->setLoginForm($container->get('zfcuser_login_form'));
+        $controller->setRegisterForm($container->get('zfcuser_register_form'));
+        $controller->setUserService($container->get('zfcuser_user_service'));
+
         return $controller;
     }
 }
